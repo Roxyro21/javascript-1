8434a8fe7c7ad99dc696306977c49f8a4839c485
Rakhshona <rakhshon.shodizoda@maile.ru>
2024-09-12 14:26:42 +0500

    did transformer homework

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
-console.log('Hello World')
+function transformObjectToString(obj) {
+    return obj.name + " (цена: $" + obj.price + ", количество: " + obj.quantity + ")";
+}
+function transformArrayToString(products) {
+    return products.map(transformObjectToString).join(", ");
+}
+const products = [
+  { name: "Laptop", price: 999.99, quantity: 3 },
+  { name: "Phone", price: 499.99, quantity: 10 },
+  { name: "Tablet", price: 299.99, quantity: 5 },
+];
+
+console.log(transformArrayToString(products));
