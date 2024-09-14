c8c5a18c416980ff8edd3eea61e04681c7cb1c29
Rakhshona <rakhshon.shodizoda@maile.ru>
2024-09-14 16:54:58 +0500

    delete comment

--- app.js
+++ app.js
@@ -1,29 +1,23 @@
-// 1. Переменная с использованием var:
-var currentStock = 100; // Текущий запас товара (может изменяться)
+var currentStock = 100;
 
-// 2. Переменная с использованием let:
-let dailyStock = 0; // Количество поступивших товаров за день (может изменяться)
+let dailyStock = 0
+const maxStock = 500; 
 
-// 3. Константа с использованием const:
-const maxStock = 500; // Максимальная емкость склада (не может изменяться)
-
-// Пример работы с переменными:
 console.log("Текущий запас:", currentStock);
 console.log("Поступившие товары за день:", dailyStock);
 console.log("Максимальная емкость:", maxStock);
 
-// Поступление новых товаров:
-dailyStock += 50; // Добавьте 50 товаров
+
+dailyStock += 50; 
 console.log("Поступившие товары за день:", dailyStock);
 
-// Продажа товара:
-currentStock -= 20; // Продали 20 товаров
+
+currentStock -= 20; 
 console.log("Текущий запас:", currentStock);
 
-// Попытка превысить максимальную емкость склада:
 try {
-  currentStock += 600; // Добавить 600 товаров
-  console.log("Текущий запас:", currentStock); // Эта строка не выполнится
+  currentStock += 600; 
+  console.log("Текущий запас:", currentStock);
 } catch (error) {
   console.error("Ошибка: Превышена максимальная емкость склада!");
 }
