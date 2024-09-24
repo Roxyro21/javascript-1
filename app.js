3f38675b29349decd99247039bd33426de77412c
Rakhshona <rakhshon.shodizoda@maile.ru>
2024-09-24 14:53:45 +0500

    continue object homework

--- app.js
+++ app.js
@@ -223,3 +223,240 @@ creditAccount.deposit(1000);
 creditAccount.withdraw(2000);
 creditAccount.applyInterest();
 console.log(creditAccount.getDebt());
+
+// ### Задача: Удаление свойства из объекта
+
+const deleteProperty = (obj, propertyName) => {
+  delete obj[propertyName];
+  return obj;
+};
+const person = { name: "Bob", age: 40, city: "San Francisco" };
+console.log(deleteProperty(person, "city"));
+
+// ### Задача: Манипуляция со свойствами объекта
+const employee = {
+  name: "Rakhshona",
+  position: "Project Manager",
+  salary: 3000,
+  isFullTime: true,
+};
+
+employee.position = "Front-end Developer";
+employee.hireDate = "20.02.2020";
+delete employee.salary;
+
+console.log(employee);
+
+// ### Задача: Создание объекта с информацией о фильме
+
+const myFilms = {
+  title: "Клинок рассекающий демонов",
+  director: "Харуо Сотодзаки",
+  year: 2022,
+  genres: ["аниме", "боевика", "фэнтези"],
+  isWatched: true,
+};
+console.log(myFilms);
+
+// ### Задача: Проверка существования свойства в объекте
+const hasProperty = (obj, propertyName) => propertyName in obj;
+
+const cars = { brand: "Toyota", model: "Camry", year: 2021 };
+console.log(hasProperty(cars, "model"));
+console.log(hasProperty(cars, "color"));
+
+// ### Задача: Итерация по объектам с помощью for...in
+const laptop = {
+  brand: "IOS",
+  model: "MacBook",
+  year: 2023,
+  price: 20000,
+};
+for (const key in laptop) {
+  console.log(`${key}: ${laptop[key]}`);
+}
+
+// ### Задача: Работа с дескрипторами свойств
+
+const personn = {
+  name: "Roxy",
+  age: 21,
+};
+Object.defineProperty(personn, "age", {
+  writable: false,
+  enumerable: false,
+  configurable: false,
+});
+
+personn.age = 22;
+console.log(personn.age);
+
+const infoPerson = Object.getOwnPropertyDescriptor(personn, "age");
+
+console.log(infoPerson);
+// ### Задача: Манипуляция со свойствами объекта
+const product = {
+  name: "Rafaello",
+  price: 50,
+  category: "chocolate",
+  inStock: false,
+};
+
+console.log(product);
+
+product.price = 55;
+product.inStock = true;
+product.rating = 5;
+delete product.category;
+console.log(product);
+
+// ### Задача: Защита объектов от изменения
+const myProject = {
+  name: "Managers 3.0",
+  team: ["Rakhshon", "Shahzod", "Ismoil"],
+  status: "Сurrent",
+};
+
+Object.preventExtensions(myProject);
+
+myProject.deadline = "10.02.25";
+console.log(myProject);
+
+Object.seal(myProject);
+myProject.status = "not relevant";
+
+delete myProject.team;
+console.log(myProject); //статус изменился
+
+Object.freeze(myProject);
+
+myProject.name = "SMS Gate";
+console.log(myProject); //не изменился
+
+// ### Задача: Прототипы в JavaScript
+
+function Person(firstName, lastName) {
+  this.firstName = firstName;
+  this.lastName = lastName;
+}
+Person.prototype.getFullName = function () {
+  return `${this.firstName} ${this.lastName}`;
+};
+
+const person1 = new Person("Rakhshon", "Shadizade");
+
+const employyee = Object.create(person1);
+
+employyee.position2 = "Manager";
+employyee.position3 = "Full steck - Developer";
+
+console.log(person1.getFullName());
+console.log(employyee.getFullName());
+console.log(employyee.position3);
+
+// ### Задача: Управление корзиной одного товара
+const shoppingCart = {
+  itemName: "",
+  itemPrice: 0,
+  itemQuantity: 0,
+
+  updateItem(name, price, quantity) {
+    this.itemName = name;
+    this.itemPrice = price;
+    this.itemQuantity = quantity;
+
+    console.log(
+      `Товар обновлен: ${this.itemName}, количество: ${this.itemQuantity}, цена: ${this.itemPrice}`
+    );
+  },
+  resetItem() {
+    this.itemName = "";
+    this.itemPrice = 0;
+    this.itemQuantity = 0;
+  },
+  getTotal() {
+    return this.itemPrice * this.itemQuantity;
+  },
+  displayCart() {
+    console.log(
+      this.itemName == ""
+        ? "Корзина пуста."
+        : `Товар: ${this.itemName}, количество: ${this.itemQuantity}, цена: ${this.itemPrice}`
+    );
+  },
+};
+shoppingCart.updateItem("Ноутбук", 999.99, 1);
+shoppingCart.displayCart();
+shoppingCart.resetItem();
+shoppingCart.displayCart(); // Ожидаемый вывод: Корзина пуста.
+
+// ### Задача: Итерация по объектам с помощью Object.keys()
+
+const student = {
+  name: "Roxy",
+  age: 21,
+  major: "Biology",
+  gpa: 96,
+};
+
+const keys = Object.keys(student);
+console.log(keys.length);
+
+keys.forEach((key) => {
+  console.log(`${key}: ${student[key]}`);
+});
+
+// ### Задача: Обновление свойства объекта
+function updateProductQuantity(product, newQuantity) {
+  product.quantity = newQuantity;
+  return product;
+}
+const products = { name: "Laptop", price: 999.99, quantity: 5 };
+
+console.log("задача1", updateProductQuantity(products, 10));
+
+// ### Задача: Деструктурирование объектов
+const usser = {
+  username: "Jonona",
+  email: "j.jonona@team.alif.tj",
+  age: 2001,
+  country: "",
+};
+
+const { username, email, age, countryy = "Tajikistan" } = usser;
+
+console.log(username);
+console.log(email);
+console.log(age);
+console.log(countryy);
+
+// ### Задача: Паттерн Фабрика (Factory Pattern)
+
+function getVehicleFactory(type) {
+  switch (type) {
+    case "Car":
+      return {
+        type: "Car",
+        wheels: 4,
+        engine: "v4",
+      };
+    case "Bike":
+      return {
+        type: "Bike",
+        wheels: 2,
+      };
+    case "Truck":
+      return {
+        type: "Truck",
+        wheels: 6,
+        loadCapaciti: 10,
+      };
+    default:
+      console.log("IDK your type");
+  }
+}
+
+console.log(getVehicleFactory("Car"));
+console.log(getVehicleFactory("Bike"));
+console.log(getVehicleFactory("Truck"));
+console.log(getVehicleFactory("BMW"));
